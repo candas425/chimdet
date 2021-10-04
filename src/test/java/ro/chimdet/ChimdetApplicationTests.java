2285f562faa35992fd332f5800c253a8f8d5d023
Lucian D <lucian.davidescu@codemart.ro>
2021-10-04 20:59:20 +0300

    Add all

---
+++ src/test/java/ro/chimdet/ChimdetApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.chimdet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimdetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
