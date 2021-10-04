da8a3a705b18de85d81cae48baa38def12cca69b
Lucian D <lucian.davidescu@codemart.ro>
2021-10-04 20:23:00 +0300

    remove

--- src/main/java/ro/utcn/backend/model/enums/TipFisier.java
+++
@@ -1,8 +0,0 @@
-package ro.utcn.backend.model.enums;
-
-/**
- * Created by Lucian on 5/20/2017.
- */
-public enum TipFisier {
-    XLXS, PDF
-}
--- src/main/java/ro/utcn/backend/model/enums/TipProdus.java
+++
@@ -1,9 +0,0 @@
-package ro.utcn.backend.model.enums;
-
-/**
- *
- * Created by Lucian on 3/26/2017.
- */
-public enum TipProdus {
-    SAPUN,DETERGENT
-}
